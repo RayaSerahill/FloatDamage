bcf5cc7d3c5e339105e6da508cd8a4f94639d705
RayaSerahill <ville@daini.dev>
2024-01-09 15:41:40 +0200

    Added some configuration features and updated API to 1.20

--- src/main/java/net/serahill/floatdamage/DmgManager.java
+++ src/main/java/net/serahill/floatdamage/DmgManager.java
@@ -2,53 +2,70 @@ package net.serahill.floatdamage;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
-import org.bukkit.entity.ArmorStand;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.Random;
 
 import static net.serahill.floatdamage.FloatDamage.plugin;
 
 public class DmgManager implements Listener {
+
+    FileConfiguration config;
+    ConfigurationSection enabledEntities;
+
+    public DmgManager(FloatDamage plugin) {
+        config = plugin.getConfig();
+        enabledEntities = config.getConfigurationSection("enabled");
+    }
+
     @EventHandler
     public void onDamage(EntityDamageByEntityEvent event) {
         Entity attacker = event.getDamager();
         Entity defender = event.getEntity();
-        if ( !(attacker instanceof Player)) return;
-        if ( (defender instanceof ArmorStand) ) {
-            if (((ArmorStand) defender).isInvisible() && defender.isInvulnerable()) {
-                event.setCancelled(true);
-            }
-            return;
-        }
 
-        org.bukkit.util.Vector vector = new org.bukkit.util.Vector(0, 0.35, 0);
-        double x = defender.getLocation().getX();
-        double y = defender.getLocation().getY();
-        double z = defender.getLocation().getZ();
-        Location loc = new Location(defender.getWorld(), x, y+0.5 ,z);
-        ArmorStand as = (ArmorStand) defender.getWorld().spawn(loc, ArmorStand.class, armorStand -> {
-            armorStand.setInvulnerable(true);
-            armorStand.setInvisible(true);
-            armorStand.setVelocity(vector);
-            armorStand.setSmall(true);
-            armorStand.setCustomName(ChatColor.RED + " " + String.valueOf(round(event.getFinalDamage(), 1)));
-            armorStand.setCustomNameVisible(true);
-            armorStand.setNoDamageTicks(40);
-            armorStand.setRemoveWhenFarAway(true);
-            armorStand.setCollidable(false);
-        });
+        if ( !(attacker instanceof Player)) return;
+        if ( !(defender instanceof Mob) && !(defender instanceof Player) ) return;
+
+        boolean enabledMonsters = enabledEntities.getBoolean("monster");
+        boolean enabledAnimal = enabledEntities.getBoolean("animal");
+        boolean enabledPlayer = enabledEntities.getBoolean("player");
+
+        // Check if defender is an animal and if animal damage is enabled, return if both are true
+        if ( (defender instanceof Animals) && !(enabledAnimal) ) return;
+
+        // Check if defender is a monster and if animal damage is enabled, return if both are true
+        if ( (defender instanceof Monster) && !(enabledMonsters) ) return;
+
+        // Check if defender is a player and if animal damage is enabled, return if both are true
+        if ( (defender instanceof Player) && (!enabledPlayer) ) return;
+        if ( (defender instanceof ArmorStand) ) return;
+
+        Random rand = new Random();
+        Location loc = defender.getLocation().clone();
+        loc.add(new Vector(.5 * (rand.nextBoolean() ? -1 : 0), 1.9f, .5 * (rand.nextBoolean() ? -1 : 0)));
+
+        String prefix = colorize(config.getString("prefix"));
+        String suffix = colorize(config.getString("suffix"));
+
+        TextDisplay floatDisplay = defender.getWorld().spawn(loc, TextDisplay.class);
+        floatDisplay.setText(prefix + String.valueOf(round(event.getFinalDamage(), 1)) + suffix);
+        floatDisplay.setBillboard(Display.Billboard.VERTICAL);
+        floatDisplay.setSeeThrough(false);
+        floatDisplay.setShadowed(false);
 
         new BukkitRunnable(){
             @Override
             public void run(){
-                as.remove();
+                floatDisplay.remove();
             }
         }.runTaskLater(plugin, 10);
     }
@@ -58,6 +75,15 @@ public class DmgManager implements Listener {
 
         BigDecimal bd = BigDecimal.valueOf(value);
         bd = bd.setScale(places, RoundingMode.HALF_UP);
+
         return bd.doubleValue();
+    }
+
+    public static String colorize(String text) {
+        if (text == null) {
+            return null;
+        }
+
+        return ChatColor.translateAlternateColorCodes('&', text);
     }
 }
--- src/main/java/net/serahill/floatdamage/FloatDamage.java
+++ src/main/java/net/serahill/floatdamage/FloatDamage.java
@@ -1,13 +1,22 @@
 package net.serahill.floatdamage;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class FloatDamage extends JavaPlugin {
 
     public static FloatDamage plugin;
+    private DmgManager DmgManager;
+
     @Override
     public void onEnable() {
         plugin = this;
-        getServer().getPluginManager().registerEvents(new DmgManager(), this);
+
+        this.saveDefaultConfig();
+
+        this.DmgManager = new DmgManager(this);
+
+        getServer().getPluginManager().registerEvents(DmgManager, this);
+
     }
 }
